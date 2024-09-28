87a4adcc00cbf10880ce7b413fe7f1feca0c4800
Bartosz Garstka <uxui.garstecki@gmail.com>
2024-09-28 05:22:48 +0200

    update

--- js/analitics.js
+++ js/analitics.js
@@ -22,3 +22,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
     a.appendChild(r);
 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+
+myUndefinedFunction()
