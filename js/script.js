48d252db8630f28e328a248e783ae9abecb442d1
Bartosz <uxui.garstecki@gmail.com>
2024-10-07 12:54:09 +0200

    update

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     themeToggleButton.addEventListener('click', () => {
         const currentTheme = document.documentElement.getAttribute('data-theme');
-        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+        const newTheme = currentTheme === 'light' ? 'dark' : 'dark';
         setTheme(newTheme);
     });
 
